ffb20be850ff8555d0e7a9d418c2461845e5191c
Himanshu Mishra <himanshu.f.mishra@accenture.com>
2023-09-01 18:11:45 +0900

    created the variables

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-    .connect(`mongodb+srv://himanshu:htEv3RmLfO5cnrLM@cluster0.vmc7hfy.mongodb.net/mern-prod?retryWrites=true&w=majority`)
+    .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vmc7hfy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
     .then(() => { 
         app.listen(process.env.PORT || 5000);
     })
