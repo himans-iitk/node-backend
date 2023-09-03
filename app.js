e13f38d92ef13d991bf34e6298bc67b4ddda7559
Himanshu Mishra <himanshu.f.mishra@accenture.com>
2023-09-03 19:55:31 +0900

    Cors issue

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
-const HttpError = require('./models/http-error')
+const HttpError = require('./models/http-error');
+
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -23,6 +23,8 @@ app.use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
     next();
 });
+
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
